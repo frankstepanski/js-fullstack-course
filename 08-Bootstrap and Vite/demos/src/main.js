60c542051ca65bc5b98338368d37e24fa4beb5e0
frankstepanski <fsjay@hotmail.com>
2026-03-18 11:46:02 -0400

    updated Bootstrap title

--- 08-Bootstrap and Vite/demos/src/main.js
+++ 08-Bootstrap and Vite/demos/src/main.js
@@ -1,6 +1,6 @@
 // Bootstrap CSS + JS
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 // Your own CSS 
 import "./styles.css";
